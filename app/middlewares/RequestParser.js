69cd2e8415a7bd13f2a820f424601d9638a87c30
henil0604 <henilmalaviya06@gmail.com>
2021-09-11 09:06:51 +0530

    added req.responseCode, which will share all Response Codes

--- app/middlewares/RequestParser.js
+++ app/middlewares/RequestParser.js
@@ -1,11 +1,13 @@
 const mongo = require("../helpers/mongo");
 const resolveRequest = require("../helpers/resolveRequest");
 const createResponse = require("../helpers/createResponse");
+const responseCode = require("../data/response_code");
 
 module.exports = async (req, res, next) => {
 
     req.createResponse = createResponse;
     req.resolve = resolveRequest(req, res)
+    req.responseCode = responseCode;
 
     next()
 }
