992bae6232ee90171de3fa88a8b08a42deb6b56b
henil0604 <henilmalaviya06@gmail.com>
2021-09-11 09:05:04 +0530

    Improved ExitHandler

--- app/helpers/Initialize_MongoDb.js
+++ app/helpers/Initialize_MongoDb.js
@@ -3,7 +3,13 @@ const mongo = require("./mongo");
 const log = require("./log");
 const prefix = "M"
 
-module.exports = async () => {
+async function connect() {
+
+    const exitHandler = async () => {
+        await mongoose.connection.close();
+        process.exit(0);
+    }
+
     try {
         if (mongoose.connection.readyState != 0) {
             log("Closed Existing Connection", "info", prefix)
@@ -25,12 +31,16 @@ module.exports = async () => {
             log("Disconnected", "info", prefix)
         })
 
-        process.on("SIGINT", async () => {
-            await mongoose.connection.close();
-            process.exit(0);
+        const crashEvents = [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `SIGTERM`];
+
+        crashEvents.forEach((eventType) => {
+            process.on(eventType, exitHandler.bind(null, eventType));
         })
 
     } catch (e) {
+        log(`ERROR: ${e.message}`, "error", prefix)
         log("Error occurred While Initializing Mongo", "error", prefix)
     }
 }
+
+module.exports = connect;
