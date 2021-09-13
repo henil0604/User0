c839f0afa1cbcd834ec65a0f6e906c61aa4e63fd
henil0604 <henilmalaviya06@gmail.com>
2021-09-13 19:13:56 +0530

    added "$user.update" method to directly update a user

--- app/helpers/user/get.js
+++ app/helpers/user/get.js
@@ -1,6 +1,13 @@
 const User = require('../../Models/User');
+const updateUser = require("./update");
 
+module.exports = async (data) => {
+    let $user = await User.findOne(data) || null;
 
-module.exports = (data) => {
-    return User.findOne(data) || null;
+    $user.__proto__.update = async (callback) => {
+        const updated = await updateUser({ userId: $user.userId }, callback);
+        return updated;
+    }
+
+    return $user;
 }
