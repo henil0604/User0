556dd927a59bb90d855d9a49e889bc4bfdcded31
henil0604 <henilmalaviya06@gmail.com>
2021-09-14 15:22:24 +0530

    fixed "null" bug

--- app/helpers/user/get.js
+++ app/helpers/user/get.js
@@ -4,6 +4,10 @@ const updateUser = require("./update");
 module.exports = async (data) => {
     let $user = await User.findOne(data) || null;
 
+    if (!$user) {
+        return null;
+    }
+
     $user.__proto__.update = async (callback) => {
         const updated = await updateUser({ userId: $user.userId }, callback);
         return updated;
