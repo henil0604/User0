ab3589999b807d66901d25863ed42c97f2cd962e
henil0604 <henilmalaviya06@gmail.com>
2021-09-14 15:22:13 +0530

    fixed "null" bug

--- app/helpers/role/get.js
+++ app/helpers/role/get.js
@@ -4,6 +4,10 @@ const updateRole = require("./update");
 module.exports = async (data) => {
     let $role = await Role.findOne(data) || null;
 
+    if (!$role) {
+        return null;
+    }
+
     $role.__proto__.update = async (callback) => {
         const updated = await updateRole({ roleId: $role.roleId }, callback);
         return updated;
