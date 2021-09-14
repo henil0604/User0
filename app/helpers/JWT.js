2f3f6bb5a6bb2e699284057e87e2e0bc2e02a5a6
henil0604 <henilmalaviya06@gmail.com>
2021-09-14 15:11:50 +0530

    added "verify" object

--- app/helpers/JWT.js
+++ app/helpers/JWT.js
@@ -3,37 +3,59 @@ const env = require("./env")
 
 let JWT = {};
 
-if(!env("ACCESS_TOKEN_PRIVATE_KEY")){
+if (!env("ACCESS_TOKEN_PRIVATE_KEY")) {
     throw new Error("ACCESS_TOKEN_PRIVATE_KEY not found");
 }
 
-if(!env("REFRESH_TOKEN_PRIVATE_KEY")){
+if (!env("REFRESH_TOKEN_PRIVATE_KEY")) {
     throw new Error("REFRESH_TOKEN_PRIVATE_KEY not found");
+}
+
+const keys = {
+    accessToken: env("ACCESS_TOKEN_PRIVATE_KEY"),
+    refreshToken: env("REFRESH_TOKEN_PRIVATE_KEY")
 }
 
 
 JWT.generate = {};
 
-JWT.generate.accessToken = (data, expiresIn=env("ACCESS_TOKEN_DEFAULT_EXPIRE_TIME")) => {
+JWT.generate.accessToken = (data, expiresIn = env("ACCESS_TOKEN_DEFAULT_EXPIRE_TIME")) => {
     let options = {}
 
-    if(expiresIn){
+    if (expiresIn) {
         options.expiresIn = expiresIn;
     }
 
-    return jwt.sign(data, env("ACCESS_TOKEN_PRIVATE_KEY"), options);
+    return jwt.sign(data, keys.accessToken, options);
 }
 
-JWT.generate.refreshToken = (data, expiresIn=env("REFRESH_TOKEN_DEFAULT_EXPIRE_TIME")) => {
+JWT.generate.refreshToken = (data, expiresIn = env("REFRESH_TOKEN_DEFAULT_EXPIRE_TIME")) => {
     let options = {}
 
-    if(expiresIn){
+    if (expiresIn) {
         options.expiresIn = expiresIn;
     }
 
-    return jwt.sign(data, env("REFRESH_TOKEN_PRIVATE_KEY"), options);
+    return jwt.sign(data, keys.refreshToken, options);
 }
 
+JWT.verify = {}
+
+JWT.verify.accessToken = (token) => {
+    try {
+        return jwt.verify(token, keys.accessToken)
+    } catch (e) {
+        return null;
+    }
+}
+
+JWT.verify.refreshToken = (token) => {
+    try {
+        return jwt.verify(token, keys.refreshToken)
+    } catch (e) {
+        return null;
+    }
+}
 
 
 
